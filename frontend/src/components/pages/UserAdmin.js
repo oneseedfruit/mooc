36601204dceb03a7ca8e15d49e3dcfe5fa5cf74a
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 03:34:32 +0800

    Search users on user admin page

--- frontend/src/components/pages/UserAdmin.js
+++ frontend/src/components/pages/UserAdmin.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useCallback } from 'react';
-import { Tab, Container } from '@material-ui/core';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Grid, TextField, Tab, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import HorizontalTabs from '../HorizontalTabs';
 import TabPanel from '../TabPanel';
@@ -20,11 +20,15 @@ const UserAdmin = ({ allAccounts, setAllAccounts, getProfileData, setNotificatio
       }));
     const classes = useStyles();
 
+    const [searchField, setSearchField] = useState('');
+    const [search, setSearch] = useState(null);
+
     const getAllAccounts = useCallback(async () => {
         try {                                    
             const data = await account.getAllAccounts();            
             
             setAllAccounts(data);
+            setSearch(data);
 
             return;                
         } catch (exception) {
@@ -41,6 +45,30 @@ const UserAdmin = ({ allAccounts, setAllAccounts, getProfileData, setNotificatio
         getAllAccounts();    
     }, [getAllAccounts]);
 
+    const handleSearch = event => {
+        const s = event.target.value.toLowerCase();
+
+        if (allAccounts) {
+            const filteredSearch = allAccounts.filter(a => {
+                const s1 = a.username.toLowerCase();
+                const s2 = a.email.toLowerCase();
+                const s3 = a.name.toLowerCase();
+
+                return s1.includes(s) ||
+                       s2.includes(s) ||
+                       s3.includes(s) ||
+                       s.includes(a.user_id);
+            });
+            setSearch(filteredSearch);
+        }
+
+        if (searchField === '') {
+            setSearch(allAccounts);
+        }
+
+        setSearchField(s);
+    };
+
     const Tabs = a11yProps => {
         return (
             [
@@ -54,8 +82,20 @@ const UserAdmin = ({ allAccounts, setAllAccounts, getProfileData, setNotificatio
             <>
                 <TabPanel value={value} index={0}>
                     <Container component="main" maxWidth="xl">
-                        <div className={classes.paper}>                            
-                            <PaginatedTable rows={ allAccounts ? allAccounts : [] } retrieve={ getAllAccounts } refresh={ getProfileData } />
+                        <div className={classes.paper}>                        
+                            <Grid container>            
+                                <TextField  id="standard-basic" 
+                                            label="search" 
+                                            value={ searchField }
+                                            name="name"
+                                            onChange={ handleSearch }
+                                            margin="normal"                                                                                                                        
+                                            autoFocus
+                                            fullWidth
+                                />                                
+                            </Grid>
+                        
+                            <PaginatedTable rows={ search ? search : [] } retrieve={ getAllAccounts } refresh={ getProfileData } />
                         </div>
                     </Container>
                 </TabPanel>
